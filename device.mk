e045e22b3a29c5991131be740b6a8fe52444d52e
HyperNotAryanX97 <ahirearyan2016@gmail.com>
2024-06-24 13:24:28 +0530

    begonia: reconfigure dalvik and dexopt
    Signed-off-by: HyperNotAryanX97 <ahirearyan2016@gmail.com>
    Signed-off-by: SHADOW <33841315+SHADOW-REX@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -80,6 +80,28 @@ PRODUCT_PACKAGES += \
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI
 
+WITH_DEXPREOPT := true
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := speed-profile
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+DONT_DEXPREOPT_PREBUILTS := false
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
+USE_DEX2OAT_DEBUG := false
+WITH_DEXPREOPT_DEBUG_INFO := false
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.boot-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.boot-dex2oat-threads=8 \
+    dalvik.vm.dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.dex2oat-threads=8 \
+    dalvik.vm.image-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.image-dex2oat-threads=8 \
+    dalvik.vm.background-dex2oat-threads=4 \
+    dalvik.vm.background-dex2oat-cpu-set=0,1,2,3,4,5,6,7,8
+
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts
